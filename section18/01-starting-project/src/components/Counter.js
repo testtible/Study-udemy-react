9f7cc7c30ec229f00cfc7278bd60160f62e33d2e
herbert.kwon <herbert.kwon@honeynaps.com>
2022-08-24 19:53:24 +0900

    update redux

--- section18/01-starting-project/src/components/Counter.js
+++ section18/01-starting-project/src/components/Counter.js
@@ -4,8 +4,11 @@ import { useSelector, useDispatch, connect } from 'react-redux';
 
 const Counter = () => {
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
@@ -18,11 +21,11 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
-  
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment by 5</button>
